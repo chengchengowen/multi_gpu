bfc015487fe27dec206e80f97b9887ea1856727a
chengchengowen <chengchengowen@hotmail.com>
2015-08-08 14:02:24 +0800

    normalize

--- multi_gpu/include/caffe/common_layers.hpp
+++ multi_gpu/include/caffe/common_layers.hpp
@@ -361,6 +361,37 @@ class MVNLayer : public Layer<Dtype> {
   Dtype eps_;
 };
 
+
+/**
+ * @brief Normalizes input.
+ */
+template <typename Dtype>
+class NormalizeLayer : public Layer<Dtype> {
+ public:
+  explicit NormalizeLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "NORMALIZE"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  Blob<Dtype> sum_multiplier_, norm_, squared_;
+};
+
+
+
 /*
  * @brief Reshapes the input Blob into an arbitrary-sized output Blob.
  *
